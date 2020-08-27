f4b055d24076b91f142e55d30bf851919dbe02f7
weixinme <imshouxin@qq.com>
2020-08-27 18:18:02 +0800

    Regression tree CART algorithm

---
+++ chapter09/regTrees.py
@@ -0,0 +1,174 @@
+from numpy import *
+
+def loadDataSet(filename):
+    dataMat = []
+    fr = open(filename)
+    for line in fr.readlines():
+        curLine = line.strip().split('\t')
+        fltLine = []
+        for i in curLine:
+            fltLine.append(float(i))
+        dataMat.append(fltLine)
+
+    return dataMat
+
+
+def binSplitDataSet(dataSet, feature, value):
+    mat0 = dataSet[nonzero(dataSet[:, feature] > value)[0], :]
+    mat1 = dataSet[nonzero(dataSet[:, feature] <= value)[0], :]
+    return mat0, mat1
+
+def regLeaf(dataSet):
+    return mean(dataSet[:, -1])
+
+def regErr(dataSet):
+    return var(dataSet[:, -1]) * shape(dataSet)[0]
+
+def chooseBestSplit(dataSet, leafType=regLeaf, errType =regErr, ops=(1, 4)):
+    tolS = ops[0]
+    tolN = ops[1]
+
+    if len(set(dataSet[:, -1].T.tolist()[0])) == 1:
+        return None, leafType(dataSet)
+
+    m, n = shape(dataSet)
+    S = errType(dataSet)
+    bestS = inf
+    bestIndex = 0
+    bestValue = 0
+
+    for featIndex in range(n-1):
+        for splitVal in set(dataSet[:, featIndex].T.A.tolist()[0]):
+            mat0, mat1 = binSplitDataSet(dataSet, featIndex, splitVal)
+            if (shape(mat0)[0] < tolN) or (shape(mat1)[0] < tolN):
+                continue
+            newS = errType(mat0) + errType(mat1)
+            if newS < bestS:
+                bestS = newS
+                bestIndex = featIndex
+                bestValue = splitVal
+
+    if (S - bestS) < tolS:
+        return None, leafType(dataSet)
+    mat0, mat1 = binSplitDataSet(dataSet, bestIndex, bestValue)
+    if (shape(mat0)[0] < tolN) or (shape(mat1)[0] < tolN):
+        return None, leafType(dataSet)
+
+    return bestIndex, bestValue
+
+def createTree(dataSet, leafType=regLeaf, errType=regErr, ops=(1, 4)):
+    feat, val = chooseBestSplit(dataSet, leafType, errType, ops)
+    if feat == None:
+        return val
+    retTree = {}
+    retTree['spInd'] = feat
+    retTree['spVal'] = val
+    lSet, rSet = binSplitDataSet(dataSet, feat, val)
+    retTree['left'] = createTree(lSet, regLeaf, regErr, ops)
+    retTree['right'] = createTree(rSet, regLeaf, regErr, ops)
+
+    return retTree
+
+def isTree(obj):
+    return (type(obj).__name__ == 'dict')
+
+def getMean(tree):
+    if isTree(tree['left']):
+        tree['left'] = getMean(tree['left'])
+    if isTree(tree['right']):
+        tree['right'] = getMean(tree['right'])
+
+    return (tree['left'] + tree['right']) / 2.0
+
+def prune(tree, testData):
+    if shape(testData)[0] == 0:
+        return getMean(tree)
+    if isTree(tree['left']) or isTree(tree['right']):
+        lSet, rSet = binSplitDataSet(testData, tree['spInd'], tree['spVal'])
+    if isTree(tree['left']):
+        tree['left'] = prune(tree['left'], lSet)
+    if isTree(tree['right']):
+        tree['right'] = prune(tree['right'], rSet)
+    if not isTree(tree['left']) and not isTree(tree['right']):
+        lSet, rSet = binSplitDataSet(testData, tree['spInd'], tree['spVal'])
+        errorNoMege = sum(power(lSet[:, -1] - tree['left'], 2)) + sum(power(rSet[:, -1] - tree['right'], 2))
+        treeMean = (tree['left'] + tree['right']) / 2.0
+        errorMerge = sum(power(testData[:, -1] - treeMean, 2))
+        if errorMerge < errorNoMege:
+            return treeMean
+        else:
+            return tree
+    else:
+        return tree
+
+def linearSolve(dataSet):
+    m, n = shape(dataSet)
+    X = mat(ones((m, n)))
+    Y = mat(ones((m, 1)))
+    X[:, 1:n] = dataSet[:, 0:n-1]
+    Y = dataSet[:, -1]
+    xTx = X.T*X
+    if linalg.det(xTx) == 0.0:
+        raise NameError('This matrix is singular, cannot do inverse, try increasing th second value of ops')
+    ws = xTx.I * (X.T * Y)
+    return ws, X, Y
+
+def modelLeaf(dataSet):
+    ws, X, Y = linearSolve(dataSet)
+    return ws
+
+def modelErr(dataSet):
+    ws, X, Y = linearSolve(dataSet)
+    yHat = X * ws
+    return sum(power(Y - yHat, 2))
+
+def regTreeEval(model, inDat):
+    return float(model)
+
+def modelTreeEval(model, inDat):
+    n = shape(inDat)[1]
+    X = mat(ones((1, n+1)))
+    X[:, 1:n+1] = inDat
+    return float(X*model)
+
+def treeForeCast(tree, inData, modelEval=regTreeEval):
+    if not isTree(tree):
+        return modelEval(tree, inData)
+    if inData[tree['spInd']] > tree['spVal']:
+        if isTree(tree['left']):
+            return treeForeCast(tree['left'], inData, modelEval)
+        else:
+            return modelEval(tree['left'], inData)
+    else:
+        if isTree(tree['right']):
+            return treeForeCast(tree['right'], inData, modelEval)
+        else:
+            return modelEval(tree['right'], inData)
+
+def createForeCast(tree, testData, modelEval=regTreeEval):
+    m = len(testData)
+    yHat = mat(zeros((m, 1)))
+    for i in range(m):
+        yHat[i, 0] = treeForeCast(tree, mat(testData[i]), modelEval)
+    return yHat
+
+if __name__ == '__main__':
+    # myDat = loadDataSet('ex2.txt')
+    # myMat = mat(myDat)
+    # myTree = createTree(myMat, ops=(0, 1))
+    # myDatTest = loadDataSet('ex2test.txt')
+    # myDatTest = mat(myDatTest)
+    # myPrunedTree = prune(myTree, myDatTest)
+    # print(myPrunedTree)
+
+    # myDat = loadDataSet('exp2.txt')
+    # myMat = mat(myDat)
+    # myTree = createTree(myMat, modelLeaf, modelErr, (1, 10))
+    # print(myTree)
+
+    trainMat = mat(loadDataSet('bikeSpeedVsIq_train.txt'))
+    testMat = mat(loadDataSet('bikeSpeedVsIq_test.txt'))
+    myTree = createTree(trainMat, ops=(1, 20))
+    yHat = createForeCast(myTree, testMat[:, 0])
+    r2 = corrcoef(yHat, testMat[:, 1], rowvar=0)[0, 1]
+    print(r2)
--- chapter09/treeExplore.py
+++ chapter09/treeExplore.py
@@ -1,70 +1,76 @@
 from numpy import *
-
-from Tkinter import *
+from tkinter import *
 import regTrees
-
 import matplotlib
-matplotlib.use('TkAgg')
+
+matplotlib.use("TkAgg")  # 设定后端为TkAgg
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from matplotlib.figure import Figure
 
-def reDraw(tolS,tolN):
-    reDraw.f.clf()        # clear the figure
-    reDraw.a = reDraw.f.add_subplot(111)
-    if chkBtnVar.get():
-        if tolN < 2: tolN = 2
-        myTree=regTrees.createTree(reDraw.rawDat, regTrees.modelLeaf,\
-                                   regTrees.modelErr, (tolS,tolN))
-        yHat = regTrees.createForeCast(myTree, reDraw.testDat, \
-                                       regTrees.modelTreeEval)
-    else:
-        myTree=regTrees.createTree(reDraw.rawDat, ops=(tolS,tolN))
+
+def reDraw(tolS, tolN):
+    reDraw.f.clf()  # 清空之前的图像
+    reDraw.a = reDraw.f.add_subplot(111)  # 重新添加子图
+    if chkBtnVar.get():  # 检查复选框是否选中，确定是模型树还是回归树
+        if tolN < 2:
+            tolN = 2
+        myTree = regTrees.createTree(reDraw.rawDat, regTrees.modelLeaf, regTrees.modelErr,
+                                            (tolS, tolN))
+        yHat = regTrees.createForeCast(myTree, reDraw.testDat, regTrees.modelTreeEval)
+    else:  # 回归树
+        myTree = regTrees.createTree(reDraw.rawDat, ops=(tolS, tolN))
         yHat = regTrees.createForeCast(myTree, reDraw.testDat)
-    reDraw.a.scatter(reDraw.rawDat[:,0], reDraw.rawDat[:,1], s=5) #use scatter for data set
-    reDraw.a.plot(reDraw.testDat, yHat, linewidth=2.0) #use plot for yHat
-    reDraw.canvas.show()
-    
-def getInputs():
-    try: tolN = int(tolNentry.get())
-    except: 
-        tolN = 10 
-        print "enter Integer for tolN"
+    reDraw.a.scatter(array(reDraw.rawDat[:, 0]), array(reDraw.rawDat[:, 1]), s=5)  # 画真实值的散点图
+    reDraw.a.plot(reDraw.testDat, yHat, linewidth=2.0)  # 画预测值的直线图
+    reDraw.canvas.draw()
+
+
+def getInputs():  # 获取用户输入的值，tolN期望得到整数值，tolS期望得到浮点数，
+    try:
+        tolN = int(tolNentry.get())  # 在Entry部件调用get方法，
+    except:
+        tolN = 10
+        print("输入int型数值作为tolN")
         tolNentry.delete(0, END)
-        tolNentry.insert(0,'10')
-    try: tolS = float(tolSentry.get())
-    except: 
-        tolS = 1.0 
-        print "enter Float for tolS"
+        tolNentry.insert(0, "10")
+    try:
+        tolS = float(tolSentry.get())
+    except:
+        tolS = 1.0
+        print("输入浮点型数值作为tolS")
         tolSentry.delete(0, END)
-        tolSentry.insert(0,'1.0')
-    return tolN,tolS
+        tolSentry.insert(0, "1.0")
+    return tolN, tolS
 
-def drawNewTree():
-    tolN,tolS = getInputs()#get values from Entry boxes
-    reDraw(tolS,tolN)
-    
-root=Tk()
 
-reDraw.f = Figure(figsize=(5,4), dpi=100) #create canvas
+def drawNewTree():  # 有人点击ReDraw按钮时就会调用该函数
+    tolN, tolS = getInputs()  # 得到输入框的值
+    reDraw(tolS, tolN)
+
+
+root = Tk()
+# Label(root, text="绘制占位符").grid(row=0, columnspan=3)  # 设置文本，第0行，距0的行值为3,
+reDraw.f = Figure(figsize=(5, 4), dpi=100)
 reDraw.canvas = FigureCanvasTkAgg(reDraw.f, master=root)
-reDraw.canvas.show()
+reDraw.canvas.draw()
 reDraw.canvas.get_tk_widget().grid(row=0, columnspan=3)
 
 Label(root, text="tolN").grid(row=1, column=0)
-tolNentry = Entry(root)
+tolNentry = Entry(root)  # Entry为允许单行文本输入的文本框，设置文本框，再定位置第1行第1列，再插入数值
 tolNentry.grid(row=1, column=1)
-tolNentry.insert(0,'10')
+tolNentry.insert(0, "10")
+
 Label(root, text="tolS").grid(row=2, column=0)
 tolSentry = Entry(root)
 tolSentry.grid(row=2, column=1)
-tolSentry.insert(0,'1.0')
-Button(root, text="ReDraw", command=drawNewTree).grid(row=1, column=2, rowspan=3)
-chkBtnVar = IntVar()
-chkBtn = Checkbutton(root, text="Model Tree", variable = chkBtnVar)
+tolSentry.insert(0, "1.0")
+
+Button(root, text="重画", command=drawNewTree).grid(row=1, column=2, rowspan=3)  # Botton按钮，设置第1行第2列，列值为3
+chkBtnVar = IntVar()  # IntVar为按钮整数值小部件
+chkBtn = Checkbutton(root, text="模型树", variable=chkBtnVar)
 chkBtn.grid(row=3, column=0, columnspan=2)
 
-reDraw.rawDat = mat(regTrees.loadDataSet('sine.txt'))
-reDraw.testDat = arange(min(reDraw.rawDat[:,0]),max(reDraw.rawDat[:,0]),0.01)
+reDraw.rawDat = mat(regTrees.loadDataSet("sine.txt"))
+reDraw.testDat = arange(min(reDraw.rawDat[:, 0]), max(reDraw.rawDat[:, 0]), 0.01)
 reDraw(1.0, 10)
-               
 root.mainloop()
